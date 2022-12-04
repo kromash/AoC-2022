02fe976d3c113a0b90858284825022361ce976d9
Kamil Romaszko <kamil.romaszko@gmail.com>
2022-12-04 19:44:43 +0100

    Add day 04 solution

---
+++ src/main/java/org/kromash/day04/Main.java
@@ -0,0 +1,72 @@
+package org.kromash.day04;
+
+import org.kromash.common.Solution;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main extends Solution {
+    public Main() {
+        super(4);
+    }
+
+    public static void main(String[] args) {
+        new Main().solve();
+    }
+
+    public String partOne() {
+        int contains = 0;
+
+        for (Section[] pair : readInput()) {
+
+            if (pair[0].contains(pair[1]) || pair[1].contains(pair[0])) {
+                contains += 1;
+            }
+        }
+
+        return String.valueOf(contains);
+    }
+
+    public String partTwo() {
+
+        int overlaps = 0;
+
+        for (Section[] pair : readInput()) {
+
+            if (pair[0].overlaps(pair[1]) || pair[1].overlaps(pair[0])) {
+                overlaps += 1;
+            }
+        }
+
+        return String.valueOf(overlaps);
+    }
+
+    private List<Section[]> readInput() {
+        List<Section[]> sections = new ArrayList<>();
+        for (var line : readInputLines()) {
+            String[] pair = line.split(",");
+
+            sections.add(Arrays.stream(pair).map(Section::fromString).toArray(Section[]::new));
+        }
+        return sections;
+    }
+
+    record Section(int start, int end) {
+        public static Section fromString(String section) {
+            String[] bound = section.split("-");
+            return new Section(Integer.parseInt(bound[0]), Integer.parseInt(bound[1]));
+        }
+
+        public boolean contains(Section section) {
+            return this.start <= section.start && this.end >= section.end;
+        }
+
+        public boolean overlaps(Section section) {
+            return (this.start <= section.start && section.start <= this.end) ||
+                    (this.start <= section.end && section.end <= this.end);
+        }
+    }
+
+
+}
---
+++ src/test/java/org/kromash/day04/MainTest.java
@@ -0,0 +1,42 @@
+package org.kromash.day04;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class MainTest {
+
+    String TEST_DATA = """
+            2-4,6-8
+            2-3,4-5
+            5-7,7-9
+            2-8,3-7
+            6-6,4-6
+            2-6,4-8
+            """;
+
+    Main main;
+
+    @BeforeEach
+    void setUp() {
+        main = mock(Main.class);
+        when(main.readInputLines()).thenReturn(List.of(TEST_DATA.split("\n")));
+        when(main.partOne()).thenCallRealMethod();
+        when(main.partTwo()).thenCallRealMethod();
+    }
+
+    @Test
+    void partOne() {
+        assertEquals("2", main.partOne());
+    }
+
+    @Test
+    void partTwo() {
+        assertEquals("4", main.partTwo());
+    }
+}
