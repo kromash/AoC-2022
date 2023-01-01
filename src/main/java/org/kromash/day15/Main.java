bd34e47b36d02cd36025e7d418c6fd0cc4862d8a
Kamil Romaszko <kamil.romaszko@gmail.com>
2023-01-01 15:55:53 +0100

    Add day 15 solution

---
+++ src/main/java/org/kromash/day15/Main.java
@@ -0,0 +1,123 @@
+package org.kromash.day15;
+
+import com.google.common.collect.Range;
+import com.google.common.collect.RangeSet;
+import com.google.common.collect.TreeRangeSet;
+import org.kromash.common.Solution;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main extends Solution {
+    public Main() {
+        super(15);
+    }
+
+    public static void main(String[] args) {
+        new Main().solve();
+    }
+
+
+    int getRowToCheck() {
+        return 2000000;
+    }
+
+    int getMaxY() {
+        return 4000000;
+    }
+
+    public String partOne() {
+
+        int rowToCheck = getRowToCheck();
+        Pattern linePattern = Pattern.compile(
+                "Sensor at x=(-?\\d+), y=(-?\\d+): closest beacon is at x=(-?\\d+), y=(-?\\d+)");
+
+        RangeSet<Integer> rangeSet = TreeRangeSet.create();
+
+        for (var line : readInputLines()) {
+            Matcher matcher = linePattern.matcher(line);
+            if (matcher.find()) {
+                Point sensor = new Point(Integer.parseInt(matcher.group(1)), Integer.parseInt(matcher.group(2)));
+                Point beacon = new Point(Integer.parseInt(matcher.group(3)), Integer.parseInt(matcher.group(4)));
+
+
+                int r = Math.abs(sensor.x - beacon.x) + Math.abs(sensor.y - beacon.y);
+
+                int dist = Math.abs(sensor.y - rowToCheck);
+                int lineCover = r - dist;
+                if (lineCover < 0)
+                    continue;
+
+                int coverStartX = sensor.x - lineCover;
+                int coverEndX = sensor.x + lineCover;
+
+
+                rangeSet.add(Range.closed(coverStartX, coverEndX));
+
+            }
+
+        }
+
+        int result = 0;
+        for (Range range : rangeSet.asRanges()) {
+            result += (Integer) range.upperEndpoint() - (Integer) range.lowerEndpoint();
+        }
+
+        return String.valueOf(result);
+    }
+
+    public String partTwo() {
+
+        int maxY = getMaxY();
+        Pattern linePattern = Pattern.compile(
+                "Sensor at x=(-?\\d+), y=(-?\\d+): closest beacon is at x=(-?\\d+), y=(-?\\d+)");
+        ArrayList<RangeSet<Integer>> coveredLines = new ArrayList<>();
+
+        for (int i = 0; i <= maxY; ++i) {
+            coveredLines.add(TreeRangeSet.create());
+        }
+
+
+        for (var line : readInputLines()) {
+            Matcher matcher = linePattern.matcher(line);
+            if (matcher.find()) {
+                Point sensor = new Point(Integer.parseInt(matcher.group(1)), Integer.parseInt(matcher.group(2)));
+                Point beacon = new Point(Integer.parseInt(matcher.group(3)), Integer.parseInt(matcher.group(4)));
+
+
+                int r = Math.abs(sensor.x - beacon.x) + Math.abs(sensor.y - beacon.y);
+
+                for (int y = Math.max(0, sensor.y - r); y <= Math.min(sensor.y + r, maxY); ++y) {
+                    int dist = Math.abs(sensor.y - y);
+                    int lineCover = r - dist;
+                    if (lineCover < 0)
+                        continue;
+                    int coverStartX = Math.max(0, sensor.x - lineCover);
+                    int coverEndX = Math.min(maxY, sensor.x + lineCover);
+                    coveredLines.get(y).add(Range.closed(coverStartX, coverEndX));
+                }
+
+
+            }
+
+        }
+        long y = 0;
+        long x = 0;
+        for (RangeSet<Integer> rangeLine : coveredLines) {
+            java.util.List<Range<Integer>> ranges = rangeLine.asRanges().stream().toList();
+            if (ranges.size() > 1) {
+                x = ranges.get(0).upperEndpoint() + 1;
+                break;
+
+            }
+            ++y;
+
+        }
+
+        return String.valueOf(4000000L * x + y);
+    }
+
+
+}
---
+++ src/test/java/org/kromash/day15/MainTest.java
@@ -0,0 +1,51 @@
+package org.kromash.day15;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class MainTest {
+    static String TEST_DATA = """
+            Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+            Sensor at x=9, y=16: closest beacon is at x=10, y=16
+            Sensor at x=13, y=2: closest beacon is at x=15, y=3
+            Sensor at x=12, y=14: closest beacon is at x=10, y=16
+            Sensor at x=10, y=20: closest beacon is at x=10, y=16
+            Sensor at x=14, y=17: closest beacon is at x=10, y=16
+            Sensor at x=8, y=7: closest beacon is at x=2, y=10
+            Sensor at x=2, y=0: closest beacon is at x=2, y=10
+            Sensor at x=0, y=11: closest beacon is at x=2, y=10
+            Sensor at x=20, y=14: closest beacon is at x=25, y=17
+            Sensor at x=17, y=20: closest beacon is at x=21, y=22
+            Sensor at x=16, y=7: closest beacon is at x=15, y=3
+            Sensor at x=14, y=3: closest beacon is at x=15, y=3
+            Sensor at x=20, y=1: closest beacon is at x=15, y=3
+            """;
+
+    Main main;
+
+    @BeforeEach
+    void setUp() {
+        main = mock(Main.class);
+        when(main.readInputLines()).thenReturn(List.of(TEST_DATA.split("\n")));
+        when(main.getRowToCheck()).thenReturn(10);
+        when(main.getMaxY()).thenReturn(20);
+        when(main.partOne()).thenCallRealMethod();
+        when(main.partTwo()).thenCallRealMethod();
+    }
+
+    @Test
+    void partOne() {
+        assertEquals("26", main.partOne());
+    }
+
+    @Test
+    void partTwo() {
+        assertEquals("56000011", main.partTwo());
+    }
+}
